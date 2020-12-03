e79bbd7e89e58578f5f9b803380be43365f08e06
AlessioScarlet <digialessio63@gmail.com>
2020-12-03 17:52:29 +0100

    Add files via upload

---
+++ copiapaginaweb.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 17:29:29 2020
+
+@author: alessiosca
+"""
+
+
+
+from pywebcopy import save_webpage
+print('ATTENZIONE:\nQUESTO PROGRAMMA FUNZIONA BENE SOLO CON SITI STATICI.\n')
+sito=input('Quale sito vuoi rubare?\n')
+punto=sito.find('.')
+slash=sito.find('/')
+nomesito=sito[slash+1:]
+url = sito
+download_folder = ''
+
+kwargs = {'bypass_robots': True, 'project_name': nomesito}
+
+save_webpage(url, download_folder, **kwargs)
