4d165a2592e21b80b54e4860d8a4357c1159132d
AlessioScarlet <digialessio63@gmail.com>
2020-12-03 17:57:52 +0100

    Add files via upload

--- copiapaginaweb.py
+++ copiapaginaweb.py
@@ -9,12 +9,13 @@ Created on Thu Dec  3 17:29:29 2020
 
 from pywebcopy import save_webpage
 print('ATTENZIONE:\nQUESTO PROGRAMMA FUNZIONA BENE SOLO CON SITI STATICI.\n')
-sito=input('Quale sito vuoi rubare?\n')
+sito=input('Quale sito vuoi rubare? (includi "http:// o https://") \n')
+cartella=input('In quale cartella lo vuoi scaricare? (INVIO per scaricarlo in C:/).\n')
 punto=sito.find('.')
 slash=sito.find('/')
 nomesito=sito[slash+1:]
 url = sito
-download_folder = ''
+download_folder = cartella
 
 kwargs = {'bypass_robots': True, 'project_name': nomesito}
 
